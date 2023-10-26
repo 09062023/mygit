eab11405b2f912ab37ee1fd9c08a2d04477ca4dc
09-06 <lhy09062023@163.com>
2023-10-26 09:51:27 +0800

    11

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./008-antd-design/01-layout";
+import App from "./009-layoutpro/layout";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
